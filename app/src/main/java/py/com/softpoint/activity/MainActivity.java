535ee1ebb7f37f76feeee1a9ecce4139966fddf6
Ever Leon <everleonpy@gmail.com>
2019-02-24 16:53:31 -0300

    Changes to be committed: 	modified:   app/src/main/java/py/com/softpoint/activity/MainActivity.java 	new file:   app/src/main/res/drawable/icon144.png 	new file:   app/src/main/res/drawable/icon150.png 	new file:   app/src/main/res/drawable/icon72.png 	new file:   app/src/main/res/drawable/password_24dp.xml 	new file:   app/src/main/res/drawable/user_24dp.xml 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/layout/activity_menu.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/py/com/softpoint/activity/MainActivity.java
+++ app/src/main/java/py/com/softpoint/activity/MainActivity.java
@@ -1,13 +1,42 @@
 package py.com.softpoint.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+        btnIngresar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+
+            case R.id.btnIngresar: {
+                Log.i("MainActivity", "Se pulzo el boton : "+v.getId());
+
+                i = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(i);
+
+            }
+
+            default: break;
+
+        }
+
     }
 }
